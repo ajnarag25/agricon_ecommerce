6dc837119a86a50a40c395ae174442ed0570905b
michaelrilan <micorilan1999@gmail.com>
2022-10-09 21:50:15 +0800

    hindi pa yan tapos hahaha

---
+++ functions.php
@@ -0,0 +1,70 @@
+<?php
+include('dbconn.php');
+
+session_start();
+$email = "avorbading@gmail.com";
+$kunwareng_login="SELECT * FROM user_info WHERE email='$email'";
+$prompt = mysqli_query($conn, $kunwareng_login);
+$getData = mysqli_fetch_array($prompt);
+$_SESSION['get_data'] = $getData;
+
+
+
+
+if (isset($_POST['add_to_cart'])) {
+    $user_id = $_SESSION['get_data']['user_id'];
+    $imagee  = $_POST['imagee'];
+    $shopname = $_POST['shop_name'];
+    $contact = $_POST['contact'];
+    $variation = $_POST['variation'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
+
+    $check_exist="SELECT id,quantity FROM cart WHERE user_id='$user_id' AND shop_name = '$shopname' AND variation = '$variation'";
+    $prompt = mysqli_query($conn, $check_exist);
+    $check = mysqli_num_rows($prompt);
+    
+    
+    if ($check == 0 or $check == null) {
+        $conn->query("INSERT INTO cart (user_id, contact, variation, quantity, price, shop_name, imagee) 
+        VALUES('$user_id','$contact','$variation', '$quantity', '$price', '$shopname', '$imagee')") or die($conn->error);
+    }
+    else {
+        $fetch = mysqli_fetch_array($prompt);
+        $id=  $fetch[0];
+        $rc = $fetch[1];
+        $int_quantity_input = (int)$quantity;
+        $int_quantity_db = (int)$rc;
+        $add_quantity = $int_quantity_db + $int_quantity_input;
+        $conn->query("UPDATE cart SET quantity= '$add_quantity' WHERE id='$id'") or die($conn->error);
+    }
+?>
+
+
+ <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script>
+          $(document).ready(function(){
+            Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Product Added to Cart',
+            showConfirmButton: false,
+            timer: 1500
+            }).then((result)=>{
+
+                window.location.href = "product_details.php";
+            })
+
+            })
+
+    
+        </script>
+
+<?php
+}
+?>
+
+
+
+
---
+++ my_account.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html lang="en">
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="images/logo.png">
+      <title>AgriCon - My Account</title>
+      <!-- CSS FILES START -->
+      <link href="css/custom.css" rel="stylesheet">
+      <link href="css/color.css" rel="stylesheet">
+      <link href="css/responsive.css" rel="stylesheet">
+      <link href="css/owl.carousel.min.css" rel="stylesheet">
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/prettyPhoto.css" rel="stylesheet">
+      <link href="css/all.min.css" rel="stylesheet">
+      <!-- CSS FILES End -->
+   </head>
+   <body>
+      <div class="wrapper">
+         <!--Header Start-->
+         <header class="header-style-2">
+            <nav class="navbar navbar-expand-lg">
+               <a class="navbar-brand" href="home.html"><img src="images/logo.png" width="200px" alt=""></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                     <li class="nav-item dropdown">
+                        <a class="nav-link" href="home.html" >Home</a>
+                     </li>
+                     <li class="nav-item"> <a class="nav-link" href="feed_home.html">Feed</a> </li>
+                     <li class="nav-item"> <a class="nav-link" href="shop_home.html">Shops</a> </li>
+                     <li class="nav-item"> <a class="nav-link" href="bidding_home.html">Bidding</a> </li>
+                     
+                  </ul>
+                  <ul class="navbar-nav mr-auto">
+                    <li> <a class="search-icon" href="#search"> <i class="fas fa-search"></i> </a> </li>
+                    <li class="dropdown">
+                        <a class="cart-icon" href="#" role="button" id="cartdropdown" data-toggle="dropdown"> <i class="fas fa-shopping-cart"></i></a>
+                        <div class="dropdown-menu cart-box" aria-labelledby="cartdropdown">
+                            Recently added item(s)
+                            <ul class="list">
+                                <li class="item">
+                                <a href="#" class="preview-image"><img class="preview" src="images/pro.jpg" alt=""></a>
+                                <div class="description"> <a href="#">Sample Product 1</a> <strong class="price">1 x P50.95</strong> </div>
+                                </li>
+                                <li class="item">
+                                <a href="#" class="preview-image"><img class="preview" src="images/pro.jpg" alt=""></a>
+                                <div class="description"> <a href="#">Sample Product 2</a> <strong class="price">2 x P144.00</strong> </div>
+                                </li>
+                            </ul>
+                            <div class="total">Total: <strong>P244.95</strong></div>
+                            <div class="view-link"><a href="#">Proceed to Checkout</a> <a href="#">View cart </a></div>
+                        </div>
+                    </li>
+                    <li class="login-reg"> <a href="my_account.html">My Account</a> | <a href="index.html">Logout</a> </li>
+                   
+                  </ul>
+               </div>
+            </nav>
+         </header>
+         <div id="search">
+            <button type="button" class="close">×</button>
+            <form class="search-overlay-form">
+               <input type="search" value="" placeholder="type keyword(s) here" />
+               <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+            </form>
+         </div>
+         <!--Header End-->
+         <!--Inner Header Start-->
+         <section class="wf100 p100 inner-header">
+            <div class="container">
+               <h1>My Account</h1>
+               <p class="text-white">User: Sample Name</p>
+            </div>
+         </section>
+         <!--Inner Header End--> 
+         <!--Contact Start-->
+         <section class="shop wf100 p80">
+            <div class="container">
+               <div class="row">
+                  <!--Pro Box Start-->
+                  <div class="col-lg-3 col-md-4">
+                    <div class="sidebar">
+                        <div class="product-box">
+                            <div class="pro-thumb">
+                            <form action="">
+                            <a href="#">
+                                <input type="file" class="btn btn-success w-100" required>
+                            </a>
+                            <img src="images/profile.png" alt=""></div>
+                            <br>
+                            <p class="text-center">Change Profile Picture</p>
+                            <button type="submit" class="btn btn-success w-100">Upload Image</button>
+                            </form>
+                        </div>
+                        <ul>
+                            <li><a href="my_account.html">My Account</a></li>
+                            <li><a href="my_purchases.html">My Purchases</a></li>
+                        </ul>
+                      
+                    </div>
+                  </div>
+                  <!--Pro Box End--> 
+                  <!--Pro Box Start-->
+                  <div class="col-lg-9 col-md-8">
+                    <h3>My Account</h3>
+                    <p>Edit your profile here</p>
+                    <div class="myaccount-form">
+                        <form>
+                            <ul class="row">
+                                <li class="col-md-6">
+                                    <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Your Name" required>
+                                    </div>
+                                </li>
+                                <li class="col-md-6">
+                                    <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Contact #" required>
+                                    </div>
+                                </li>
+                                <li class="col-md-6">
+                                    <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Email Address" required>
+                                    </div>
+                                </li>
+                                <li class="col-md-6">
+                                    <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="User Name">
+                                    </div>
+                                </li>
+                                <li class="col-md-12">
+                                    <button class="register">Save Changes</button>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                  </div>
+                  <!--Pro Box End--> 
+
+               </div>
+            </div>
+         </section>
+         <!--Footer Start-->
+         <footer class="footer">
+            <div class="footer-top wf100">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-lg-4 col-md-8 col-sm-8">
+                        <!--Footer Widget Start-->
+                        <div class="footer-widget">
+                           <h4>Company</h4>
+                           <p> AgriCon Mart is the first and only e-commerce platform exclusively in Oriental Mindoro. </p>
+                           <a href="#" class="lm" data-toggle="modal" data-target="#aboutus">About us</a> 
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="aboutus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                           <div class="modal-dialog" role="document">
+                           <div class="modal-content">
+                              <div class="modal-header">
+                                 <h5 class="modal-title" id="exampleModalLabel">About Us</h5>
+                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                 <span aria-hidden="true">&times;</span>
+                                 </button>
+                              </div>
+                              <div class="modal-body">
+                                 <p>AgriCon Mart is the first and only e-commerce platform exclusively in Oriental Mindoro. 
+                                    <br><br>
+                                    Launched in 2022, it is a platform specialized for the province, abling the customers enjoy an easy, fast, and secured online purchasing of agricultural and construction or hardware products.
+                                    <br><br>
+                                    AgriCon Mart believes that those types of products should be easily accessible. This is the vision we aspire to deliver and the Mindoreños deserve.
+                                 </p>
+                              </div>
+                           </div>
+                           </div>
+                        </div>
+                        <!--Footer Widget End--> 
+                     </div>
+                     <div class="col-lg-4 col-md-8 col-sm-8">
+                        <!--Footer Widget Start-->
+                        <div class="footer-widget">
+                           <h4>Information</h4>
+                           <ul class="">
+                              <li><a href="#">My Account</a></li>
+                              <li><a href="#">Rewards</a></li>
+                              <li><a href="#">Terms and Conditions</a></li>
+                              <li><a href="#">Buying Guide </a></li>
+                           </ul>
+                        </div>
+                        <!--Footer Widget End--> 
+                     </div>
+                     <div class="col-lg-4 col-md-8 col-sm-8">
+                        <!--Footer Widget Start-->
+                        <div class="footer-widget">
+                           <h4>Contact Us</h4>
+                           <p>Mobile: +630369450548</p>
+                           <p>Email: cristcastillo14@gmail.com</p>
+                        </div>
+                        <!--Footer Widget End--> 
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="footer-copyr wf100">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-4 col-sm-4"> <img src="images/logo.png" alt=""> </div>
+                  </div>
+               </div>
+            </div>
+         </footer>
+         <!--Footer End--> 
+      </div>
+
+      <!--   JS Files Start  --> 
+      <script src="js/jquery-3.3.1.min.js"></script> 
+      <script src="js/jquery-migrate-1.4.1.min.js"></script> 
+      <script src="js/popper.min.js"></script> 
+      <script src="js/bootstrap.min.js"></script> 
+      <script src="js/owl.carousel.min.js"></script> 
+      <script src="js/jquery.prettyPhoto.js"></script> 
+      <script src="js/isotope.min.js"></script> 
+      <script src="js/custom.js"></script>
+   </body>
+
+</html>
